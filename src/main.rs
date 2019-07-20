ad5c88d37e3fa031e79affb362c917f7149e002e
Fredrik Simonsson <simson@thesimson.net>
2019-07-20 18:47:29 +0900

    Added hyper webserver, got a HelloWorld over HTTP

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
+use hyper::{Body, Response, Server, Request};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+use hyper::service::service_fn;
+
+
+
 fn hw() ->String{
     "Hello, world!".to_string()
 }
 
+fn microservice_handler(req: Request<Body>) -> Response<Body> {
+    Response::new(Body::from(hw()))
+}
 
 fn main() {
-    println!("{}",hw());
+    let addr = ([127, 0, 0, 1], 8888).into();
+
+    let handler = ||{service_fn_ok(|req| microservice_handler(req ))};
+    let server = Server::bind(&addr)
+        .serve(handler)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    hyper::rt::run(server);
 }
+
 
 #[cfg(test)]
 mod tests {
