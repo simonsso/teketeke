d4abf8faa08cb878e55dd327d70781f3e40cc96a
Fredrik Simonsson <simson@thesimson.net>
2019-07-21 14:11:48 +0900

    Hrm does not seem to be multi threading

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use hyper::{Body, Response, Server, Request};
 use hyper::rt::Future;
+use lazy_static::lazy_static;
 use hyper::service::service_fn_ok;
-use hyper::service::service_fn;
+//TODO expect to need this: use hyper::service::service_fn;
+
+use regex::Regex;
 
 
 use std::{thread, time};
 
-
+// Function for emulating execution time and explore locking anc blocking
 fn hw() ->String{
     println!("HW start");
     let ten_sec = time::Duration::from_millis(10000);
@@ -15,26 +18,28 @@ fn hw() ->String{
     "Hello, world!".to_string()
 }
 
+lazy_static!{
+    // TODO verify the correctness of regexp in tests
+    static ref RE_TABLE_NUM: Regex = Regex::new(r"^/table/[\d+](/.*)$").unwrap();
+}
 
+
+// Encapsulate response for hyper
 fn microservice_handler(req: Request<Body>) -> Response<Body> {
-    println!("HELLO {}", req.uri());
-    let uri:String = req.uri().to_string();
-    let uri:Vec<&str> = uri.split('/').collect();
+        let ans:String = microservice_handler_inner(req.uri().to_string(), req.method().to_string());
+        Response::new(Body::from(ans))
+}
 
-    // push two empty to ensure match have some data to work with
+// Change argument for unit_tests
 
-    match(uri[0],uri[1],uri[2]){
-        ("table",_,_) => {
-            Response::new(Body::from(hw()))
-        }
-        (_,_,_)=>{
-            Response::new(Body::from(hw()))
-        }
-    }
- //   Response::new(Body::from(hw()))
+fn microservice_handler_inner(uri:String,method:String) ->String{
+    println!("{}:{}",method,uri);
+    hw()
 }
 
 fn main() {
+    
+    println!("Would you like to play a game?");
     let addr = ([127, 0, 0, 1], 8888).into();
 
     let handler = ||{service_fn_ok(|req| microservice_handler(req ))};
@@ -64,11 +69,27 @@ mod tests {
     // how to achive this.
     //
     // For now this must be tested at system level by usage.
-    // ## #[test]
-    // ##fn handler_test(){
-    // ##    let mut r:Request<()> = Default();
-    // ##    assert!(r.uri() , "/");
-    // ##    let ans = microservice_handler(r);
-    // ##    assert!(*ans.body(), *"/");
-    // ##}
+     #[test]
+    fn handler_test(){
+        let ans = microservice_handler_inner("/table/10", "GET");
+        //assert!(ans.body() , "Hello World");
+
+        let ans = microservice_handler_inner("/table","GET");
+        //assert!(*ans.body(), *"/");
+    }
+    #[test]
+    fn check_regexp(){
+        let ans = RE_TABLE_NUM.captures("/table/100/open");
+
+        match ans
+        {
+            Some(m) =>{
+                println!("Match 1{}",m.get(1).unwrap());
+                println!("Match 1{}",m.get(1).unwrap());
+            }
+            _ => {
+                assert!(false);
+            }
+        }
+    }
 }
---
+++ test_scripts/excercice001.sh
@@ -0,0 +1,13 @@
+curl -X DELETE http://localhost:8888/table/1 &
+echo 1
+curl -X DELETE http://localhost:8888/table/2 &
+echo 1
+curl -X DELETE http://localhost:8888/table/3 &
+echo 1
+curl -X DELETE http://localhost:8888/table/4 &
+echo 1
+curl -X DELETE http://localhost:8888/table/5 &
+echo 1
+echo 1
+curl -X DELETE http://localhost:8888/table/6
+
