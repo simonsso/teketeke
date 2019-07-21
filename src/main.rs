2ac3b0ec46b037a19f13ac1e4a95bc685211831a
Fredrik Simonsson <simson@thesimson.net>
2019-07-21 10:48:27 +0900

    WIP some explorations
    Confirmed server can handle two concurenet requests - good
    String spit will fail if the request is  malforamted - bad

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,34 @@ use hyper::service::service_fn_ok;
 use hyper::service::service_fn;
 
 
+use std::{thread, time};
+
 
 fn hw() ->String{
+    println!("HW start");
+    let ten_sec = time::Duration::from_millis(10000);
+    thread::sleep(ten_sec);
+    println!("HW done");
     "Hello, world!".to_string()
 }
 
+
 fn microservice_handler(req: Request<Body>) -> Response<Body> {
-    Response::new(Body::from(hw()))
+    println!("HELLO {}", req.uri());
+    let uri:String = req.uri().to_string();
+    let uri:Vec<&str> = uri.split('/').collect();
+
+    // push two empty to ensure match have some data to work with
+
+    match(uri[0],uri[1],uri[2]){
+        ("table",_,_) => {
+            Response::new(Body::from(hw()))
+        }
+        (_,_,_)=>{
+            Response::new(Body::from(hw()))
+        }
+    }
+ //   Response::new(Body::from(hw()))
 }
 
 fn main() {
@@ -24,10 +45,10 @@ fn main() {
     hyper::rt::run(server);
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn it_works() {
         assert_eq!("Hello, world!" , hw());
@@ -37,4 +58,17 @@ mod tests {
     fn ne_it_works() {
         assert_eq!("Hello, WORLD!" , hw());
     }
+
+    // TODO/Note add unit test on handler level, when there is time to add
+    // a request struct and check the response struct. Have to figure out
+    // how to achive this.
+    //
+    // For now this must be tested at system level by usage.
+    // ## #[test]
+    // ##fn handler_test(){
+    // ##    let mut r:Request<()> = Default();
+    // ##    assert!(r.uri() , "/");
+    // ##    let ans = microservice_handler(r);
+    // ##    assert!(*ans.body(), *"/");
+    // ##}
 }
