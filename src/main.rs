fa5d22b8eb31cbfd4b416b6e543e807eef5d8b3e
Fredrik Simonsson <simson@thesimson.net>
2019-07-22 21:58:03 +0900

    WIP some work with futures and RwLocks
    looks promising but still not close

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,47 @@ use hyper::{Body, Response, Server, Request, StatusCode, Method};
 use lazy_static::lazy_static;
 use hyper::service::service_fn;
 
+use std::sync::Arc;
 // added from example
 use std::io::{Error};
-use futures::{future, Future, Stream};
+use futures::{future, Future, Stream, task};
 use regex::Regex;
 
-
 use std::{thread, time};
+
+use futures_locks::RwLock;
+use tokio::timer::Interval;
+use futures::future::lazy;
+use futures::task::spawn;
+use future::{err, ok};
+
+#[derive(Copy, Clone)]
+enum States{
+    ETA(u32),
+    DONE,
+    REMOVED,
+    EMPTY,
+}
+
+struct Record{
+    id: u32,
+    state: States,
+}
+
+struct Datastore{
+    vault: Vec<RwLock<Record>>,
+}
+
+fn DatastoreRwLock()->RwLock<Datastore>{
+    let v:Vec<RwLock<Record>> = Vec::with_capacity(100);
+    let d:Datastore = Datastore{vault : v};
+    RwLock::new(d)
+}
+
 
 // Function for emulating execution time and explore locking anc blocking
 fn hw() ->String{
     println!("HW start{:?}",thread::current().id() );
-    let ten_sec = time::Duration::from_millis(1000);
-    //thread::sleep(ten_sec);
     println!("HW done {:?}",thread::current().id());
     "Hello, world!".to_string()
 }
@@ -23,25 +51,15 @@ fn hw() ->String{
 lazy_static!{
     // TODO verify the correctness of regexp in tests
     static ref RE_TABLE_NUM: Regex = Regex::new(r"^/table/(\d+)(/.*)?$").unwrap();
+    static ref STORAGE:RwLock<Datastore> = DatastoreRwLock();
 }
 
 
 // Encapsulate response for hyper
 fn microservice_handler(req: Request<Body>) -> Box<Future<Item=Response<Body>, Error=Error> + Send> {
-        let ans:String = microservice_handler_inner(req.uri().to_string(), req.method().to_string());
-        
-        let resp = Response::builder()
-            .status(200)
-            .body(Body::from(ans))
-            .unwrap();
-        Box::new(future::ok(resp))
-}
-
-// Change argument for unit_tests
-
-fn microservice_handler_inner(uri:String,method:String) ->String{
-    println!("{}:{}",method,uri);
-
+    let uri:String = req.uri().to_string();
+    let method = req.method().to_string();
+    
     let (table,path):(Option<u32>,Option<String>) =  match RE_TABLE_NUM.captures(&uri){
         Some(m)=>{
             // this is checked to be an integer
@@ -60,11 +78,21 @@ fn microservice_handler_inner(uri:String,method:String) ->String{
         }
     };
     
-
-
     match (method.as_ref(),table,path){
         ("GET",Some(t),None) =>{
             // GET all items for table t
+            let lock = STORAGE.read().map(|guard| { *guard });
+            let mut v = spawn(lock).wait_future().unwrap().vault;
+            match v.get(t as usize) {
+                Some(x) => {
+                    println!("Found Gold in {}",t);
+                }
+                None =>{
+                    let r = Record{id:t, state: States::ETA(t) };
+                    v.push(RwLock::new(r));
+                }
+
+            }
         }
         ("GET",None,None) =>{
             // Get all items
@@ -82,14 +110,24 @@ fn microservice_handler_inner(uri:String,method:String) ->String{
             // Unsupported operation
         }
     };
-    hw()
+    
+    let ans = "TODO Chnage me";
+    let resp = Response::builder()
+        .status(200)
+        .body(Body::from(ans))
+        .unwrap();
+    Box::new(future::ok(resp))
 }
 
 fn main() {
     println!("Starting server port");
     let addr = ([127, 0, 0, 1], 8888).into();
 
-    let server = Server::bind(&addr).serve(move||{service_fn(move|req| microservice_handler(req ))});
+    let server = Server::bind(&addr).serve(||{
+        service_fn(move |req|{
+                microservice_handler(req)
+            }
+        )});
 
     let server = server.map_err(drop);
     hyper::rt::run(server);
