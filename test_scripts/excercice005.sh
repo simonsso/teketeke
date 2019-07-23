ed862702843f471276ffc001b853ddb06bff1737
Fredrik Simonsson <simson@thesimson.net>
2019-07-23 13:51:44 +0900

    Added some default data

---
+++ test_scripts/excercice005.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function send_request() {
+    echo -ne "- - - - - - - - - \nRequest: $1\nResponse: "
+    curl --header "Content-Type: application/json" --request POST \
+         --data "$1" \
+         http://localhost:8888/table/2
+    echo ""
+}
+
+send_request '{"order": "order", "parameters": { "itemname": "MTIzNDU2Nzg5MA==","qty" : 100 } }'
+
