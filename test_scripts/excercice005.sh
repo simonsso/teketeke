5d50a68cfd97a36f042b425d47cf4c5891508079
Fredrik Simonsson <simson@thesimson.net>
2019-07-23 19:02:17 +0900

    Changed json format to accept array of orders,
    Not optimal but it looks ok

--- test_scripts/excercice005.sh
+++ test_scripts/excercice005.sh
@@ -8,5 +8,5 @@ function send_request() {
     echo ""
 }
 
-send_request '{"order": "order", "parameters": { "itemname": "MTIzNDU2Nzg5MA==","qty" : 100 } }'
+send_request '{"tab":[{"order": "order", "parameters": { "itemname": "MTIzNDU2Nzg5MA==","qty" : 100 }},{"order": "order", "parameters": { "itemname": "MTIzNDU2Nzg5MA==","qty" : 100 } }]}'
 
